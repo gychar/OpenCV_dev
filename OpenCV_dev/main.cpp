14d5276423323fa5c834104f5e67c4ec07eab9bc
Char <caigaoyang628@gmail.com>
2018-01-26 14:38:55 +0100

    Initial Commit

---
+++ OpenCV_dev/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV_dev
+//
+//  Created by CHAR on 2018/1/26.
+//  Copyright © 2018年 CHAR. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
